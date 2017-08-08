50650cf84ecd0165de6745badffa190389500e78
wenjia Xiong <495743376@qq.com>
2017-08-08 16:15:11 +0800

    Add events.go
    Used to add custom events.

---
+++ stress/requester/events.go
@@ -0,0 +1,36 @@
+package requester
+
+import (
+	"net/http"
+)
+
+type Events struct {
+	RequestBefore func(*Request, Share)
+	ResponseAfter func(*http.Response, Share)
+}
+
+//Share is a container that is shared in the current transaction,
+//you can access the required content.
+type Share map[string]interface{}
+
+type Request struct {
+	URLStr    string
+	Method    string
+	RoutineNo int
+	Index     int
+	body      *[]byte
+	header    http.Header
+}
+
+func (r *Request) SetBody(bodyStr string) {
+	*r.body = []byte(bodyStr)
+}
+
+func (r *Request) SetHeader(key, value string) {
+	_, ok := (r.header)[key]
+	if ok {
+		r.header.Add(key, value)
+	} else {
+		r.header.Set(key, value)
+	}
+}
